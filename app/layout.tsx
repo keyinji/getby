5663b59489c1fea8bc3ea8cf91b62abbbaed1637
keyinji <92763672+keyinji@users.noreply.github.com>
2023-07-28 20:26:39 -0400

    footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body className="">
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   )
